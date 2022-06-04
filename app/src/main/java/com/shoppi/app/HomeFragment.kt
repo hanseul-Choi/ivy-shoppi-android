e29bc2e75a73670aebb5e63e09dbecaf18a0fd50
hanseuk-Choi <hschoi5542@gmail.com>
2022-06-05 01:54:53 +0900

    Json data 파싱 작업

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import org.json.JSONObject
 
 class HomeFragment: Fragment() {
 
@@ -42,5 +43,15 @@ class HomeFragment: Fragment() {
         // requireContext() : 항상 non-null의 context를 return
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
         Log.d("homeData", homeData ?: "")
+
+        if(!homeData.isNullOrEmpty()) {
+            val jsonObject = JSONObject(homeData)
+            val title = jsonObject.getJSONObject("title")
+            val text = title.getString("text")
+            val iconUrl = title.getString("icon_url")
+
+            val titleValue = Title(text, iconUrl)
+            titleValue.text
+        }
     }
 }
