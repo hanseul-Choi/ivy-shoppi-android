3ecce59ff67ae6ead811e5b26047617b6365e200
hanseuk-Choi <hschoi5542@gmail.com>
2022-06-05 14:45:39 +0900

    과제 1-4. Glide 라이브러리 활용하여 Appbar icon 넣기

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.bumptech.glide.Glide
 import org.json.JSONObject
 import org.w3c.dom.Text
 
@@ -28,18 +30,18 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-//        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
-//
-//        // abstract의 method가 하나만 존재할 때(Single Abstract Method) => lambda로 변경 가능
-//        button.setOnClickListener {
-////            // transaction : Fragment를 추가, 삭제, 수정을 요청하는 것
-////            val transaction = parentFragmentManager.beginTransaction()
-////            transaction.add(R.id.container_main, ProductDetailFragment())
-////            transaction.commit() // transaction이 실행되기 위해서는 마지막에 commit해주어야함
-//
-//            // action의 id를 넣어 네비게이션 동작
-//            findNavController().navigate(R.id.action_home_to_product_detail)
-//        }
+        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+
+        // abstract의 method가 하나만 존재할 때(Single Abstract Method) => lambda로 변경 가능
+        button.setOnClickListener {
+//            // transaction : Fragment를 추가, 삭제, 수정을 요청하는 것
+//            val transaction = parentFragmentManager.beginTransaction()
+//            transaction.add(R.id.container_main, ProductDetailFragment())
+//            transaction.commit() // transaction이 실행되기 위해서는 마지막에 commit해주어야함
+
+            // action의 id를 넣어 네비게이션 동작
+            findNavController().navigate(R.id.action_home_to_product_detail)
+        }
 
         val assetLoader = AssetLoader()
         // requireContext() : 항상 non-null의 context를 return
@@ -54,9 +56,18 @@ class HomeFragment: Fragment() {
 
             val titleValue = Title(text, iconUrl)
 
-            // appbar title, image
+            // appbar title
             val appbarTextView = view.findViewById<TextView>(R.id.tv_fragment_home_appbar)
             appbarTextView.text = titleValue.text
+
+            // appbar icon
+            val appbarIcon = view.findViewById<ImageView>(R.id.iv_fragment_home_appbar_icon)
+
+            Log.d("icon url", titleValue.iconUrl)
+
+            Glide.with(this)
+                .load(titleValue.iconUrl)
+                .into(appbarIcon)
         }
     }
 }
