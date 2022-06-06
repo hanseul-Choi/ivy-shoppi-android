de03ebb3f8a88d2c27d3afe27307639df5d8c012
hanseuk-Choi <hschoi5542@gmail.com>
2022-06-06 19:27:02 +0900

    과제 1. fix answer

---
+++ app/src/main/java/com/shoppi/app/GlideModule.kt
@@ -0,0 +1,10 @@
+package com.shoppi.app
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+// GlideModule을 Generate하기 위해 생성만 함
+@GlideModule
+class GlideModule : AppGlideModule() {
+
+}
--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -5,16 +5,12 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import com.bumptech.glide.Glide
 import org.json.JSONObject
-import org.w3c.dom.Text
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
     // onCreateView: inflate하기 위한 callback method
     override fun onCreateView(
@@ -30,44 +26,39 @@ class HomeFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+//        val button = view.findViewById<Button>(R.id.btn_enter_product_detail)
+//
+//        // abstract의 method가 하나만 존재할 때(Single Abstract Method) => lambda로 변경 가능
+//        button.setOnClickListener {
+////            // transaction : Fragment를 추가, 삭제, 수정을 요청하는 것
+////            val transaction = parentFragmentManager.beginTransaction()
+////            transaction.add(R.id.container_main, ProductDetailFragment())
+////            transaction.commit() // transaction이 실행되기 위해서는 마지막에 commit해주어야함
+//
+//            // action의 id를 넣어 네비게이션 동작
+//            findNavController().navigate(R.id.action_home_to_product_detail)
+//        }
 
-        // abstract의 method가 하나만 존재할 때(Single Abstract Method) => lambda로 변경 가능
-        button.setOnClickListener {
-//            // transaction : Fragment를 추가, 삭제, 수정을 요청하는 것
-//            val transaction = parentFragmentManager.beginTransaction()
-//            transaction.add(R.id.container_main, ProductDetailFragment())
-//            transaction.commit() // transaction이 실행되기 위해서는 마지막에 commit해주어야함
 
-            // action의 id를 넣어 네비게이션 동작
-            findNavController().navigate(R.id.action_home_to_product_detail)
-        }
+        val toolbarTitle = view.findViewById<TextView>(R.id.toolbar_home_title)
+        val toolbarIcon = view.findViewById<ImageView>(R.id.toolbar_home_icon)
 
         val assetLoader = AssetLoader()
         // requireContext() : 항상 non-null의 context를 return
         val homeData = assetLoader.getJsonString(requireContext(), "home.json")
         Log.d("homeData", homeData ?: "")
 
-        if(!homeData.isNullOrEmpty()) {
+        if (!homeData.isNullOrEmpty()) {
             val jsonObject = JSONObject(homeData)
             val title = jsonObject.getJSONObject("title")
             val text = title.getString("text")
             val iconUrl = title.getString("icon_url")
 
-            val titleValue = Title(text, iconUrl)
+            toolbarTitle.text = text
+            GlideApp.with(this)
+                .load(iconUrl)
+                .into(toolbarIcon)
 
-            // appbar title
-            val appbarTextView = view.findViewById<TextView>(R.id.tv_fragment_home_appbar)
-            appbarTextView.text = titleValue.text
-
-            // appbar icon
-            val appbarIcon = view.findViewById<ImageView>(R.id.iv_fragment_home_appbar_icon)
-
-            Log.d("icon url", titleValue.iconUrl)
-
-            Glide.with(this)
-                .load(titleValue.iconUrl)
-                .into(appbarIcon)
         }
     }
 }
