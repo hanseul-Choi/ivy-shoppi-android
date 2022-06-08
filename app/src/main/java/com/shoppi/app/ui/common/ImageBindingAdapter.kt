b605eccc2572ce21bdc39c6eb7f188a6a8691d1d
hanseuk-Choi <hschoi5542@gmail.com>
2022-06-08 16:30:59 +0900

    rounded corner, gradient 처리

--- app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapter.kt
+++ app/src/main/java/com/shoppi/app/ui/common/ImageBindingAdapter.kt
@@ -2,6 +2,9 @@ package com.shoppi.app.ui.common
 
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
 import com.shoppi.app.GlideApp
 
 @BindingAdapter("imageUrl")
@@ -19,6 +22,26 @@ fun loadCircleImage(view: ImageView, imageUrl: String?) {
         GlideApp.with(view)
             .load(imageUrl)
             .circleCrop()
+            .into(view)
+    }
+}
+
+@BindingAdapter("r12ImageUrl")
+fun loadR12ImageUrl(view: ImageView, imageUrl: String?) {
+    if(!imageUrl.isNullOrEmpty()) {
+        GlideApp.with(view)
+            .load(imageUrl)
+            .transform(CenterCrop(), RoundedCorners(12))
+            .into(view)
+    }
+}
+
+@BindingAdapter("r8ImageUrl")
+fun loadR8ImageUrl(view: ImageView, imageUrl: String?) {
+    if(!imageUrl.isNullOrEmpty()) {
+        GlideApp.with(view)
+            .load(imageUrl)
+            .transform(CenterCrop(), RoundedCorners(8))
             .into(view)
     }
 }
